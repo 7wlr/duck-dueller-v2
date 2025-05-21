c4d3bc16eb3c7148ac0737fffa1535bb22c32800
wlrfr <7wlrrr@gmail.com>
2025-05-21 17:06:19 +0200

    WLR is now hidden from the handshake to hypixel (aka hidden from the modlist that is sent to hypixel)

--- src/main/java/bot/seven/WLR/mixins/MixinModList.java
+++ src/main/java/bot/seven/WLR/mixins/MixinModList.java
@@ -23,7 +23,7 @@ public class MixinModList {
     public void removeMod(List<ModContainer> modContainerList, CallbackInfo ci) {
         if (!Minecraft.getMinecraft().isSingleplayer()) {
             System.out.println("Removing mod from handshake...");
-            this.modTags.keySet().removeIf(key -> Objects.equals(key, "duckdueller"));
+            this.modTags.keySet().removeIf(key -> Objects.equals(key, "wlr"));
         }
     }
 
